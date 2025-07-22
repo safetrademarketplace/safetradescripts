0eb1a20d8d3378d5fa1f819294b6b9de08c431c9
safetrademarketplace <safetrademarketplace@outlook.com>
2025-07-22 17:25:37 +1000

    Update notify.lua

--- notify.lua
+++ notify.lua
@@ -3,7 +3,7 @@ local StarterGui   = game:GetService("StarterGui")
 local TweenService = game:GetService("TweenService")
 local player      = Players.LocalPlayer
 local guiParent   = player:WaitForChild("PlayerGui")
-local DURATION     = 8 
+local DURATION     = 5
 local DISCORD_LINK = "https://discord.gg/uJTqjjVbb8"
 local MESSAGE_TEXT = "This script was created by SafeTrade Marketplace and is available keyless in our Discord server."
 
